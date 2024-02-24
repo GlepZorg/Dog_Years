b3f70fc870037e599be7bc1ef82174310176c2c0
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-02-23 20:37:57 -0800

    final update

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ earlyYears *= 10.5;
 let laterYears = myAge - 2;
 
 laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Alexis'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years which is ${myAgeInDogYears} years old in dog years.`);
